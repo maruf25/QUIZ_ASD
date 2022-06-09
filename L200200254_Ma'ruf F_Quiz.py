46498267ec90e1d290b4fb2d8ecd6403e77cf3fa
Ma'ruf Fiardy <60255039+maruf25@users.noreply.github.com>
2022-06-09 11:16:40 +0700

    Add files via upload
    pertama

---
+++ L200200254_Ma'ruf F_Quiz.py
@@ -0,0 +1,57 @@
+#Nomor 1
+##import re
+##
+##username = '@25maruf_'
+##cocok = re.findall(r'@[a-z]+[0-9]+_',username)
+##if cocok:
+##    print(cocok, "pass")
+##else:
+##    print("Failed")
+
+#Nomor 2
+##import re
+##f = open('quizno2.txt')
+##teks = f.read()
+##f.close()
+##p = r'\w+@+[\w.-]+'
+##strings = re.findall(p,teks)
+##print(strings)
+
+#Nomer 3
+class Pohon(object):
+    def __init__(self,data):
+        self.data = data
+        self.kiri = None
+        self.kanan = None
+
+A = Pohon("A")
+B = Pohon("B")
+C = Pohon("C")
+D = Pohon("D")
+E = Pohon("E")
+F = Pohon("F")
+G = Pohon("G")
+H = Pohon("H")
+I = Pohon("I")
+
+A.kiri = B ; A.kanan = C
+C.kiri = D ; C.kanan = E
+E.kiri = F ; E.kanan = G
+G.kiri = H
+B.kiri = I
+
+def cetakLuarKiri(data):
+    if data is not None:
+        cetakLuarKiri(data.kiri)
+        print(data.data)
+def cetakLuarKanan(data):
+    if data.kanan is not None:
+        cetakLuarKanan(data.kanan)
+        print(data.data)
+        
+
+def cetakLuar(data):
+    cetakLuarKiri(data)
+    cetakLuarKanan(data)
+
+cetakLuar(A)
