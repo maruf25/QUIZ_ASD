5a2ab9c42c73453d90ae92f5992abe2794dfd3fc
Ma'ruf Fiardy <60255039+maruf25@users.noreply.github.com>
2022-06-09 11:43:54 +0700

    3

--- L200200254_Ma'ruf F_Quiz.py
+++ L200200254_Ma'ruf F_Quiz.py
@@ -1,7 +1,7 @@
 #Nomor 1
 ##import re
 ##
-##username = '@25maruf_'
+##username = '@maruf25_'
 ##cocok = re.findall(r'@[a-z]+[0-9]+_',username)
 ##if cocok:
 ##    print(cocok, "pass")
@@ -18,40 +18,36 @@
 ##print(strings)
 
 #Nomer 3
-class Pohon(object):
-    def __init__(self,data):
-        self.data = data
-        self.kiri = None
-        self.kanan = None
-
-A = Pohon("A")
-B = Pohon("B")
-C = Pohon("C")
-D = Pohon("D")
-E = Pohon("E")
-F = Pohon("F")
-G = Pohon("G")
-H = Pohon("H")
-I = Pohon("I")
-
-A.kiri = B ; A.kanan = C
-C.kiri = D ; C.kanan = E
-E.kiri = F ; E.kanan = G
-G.kiri = H
-B.kiri = I
-
-def cetakLuarKiri(data):
-    if data is not None:
-        cetakLuarKiri(data.kiri)
-        print(data.data)
-def cetakLuarKanan(data):
-    if data is not None:
-        print(data.data)
-        cetakLuarKanan(data.kanan)
-        
-
-def cetakLuar(data):
-    cetakLuarKiri(data)
-    cetakLuarKanan(data)
-
-cetakLuar(A)
+##class Pohon(object):
+##    def __init__(self,data):
+##        self.data = data
+##        self.kiri = None
+##        self.kanan = None
+##
+##A = Pohon("A")
+##B = Pohon("B")
+##C = Pohon("C")
+##D = Pohon("D")
+##E = Pohon("E")
+##F = Pohon("F")
+##G = Pohon("G")
+##H = Pohon("H")
+##I = Pohon("I")
+##
+##A.kiri = B ; A.kanan = C
+##C.kiri = D ; C.kanan = E
+##E.kiri = F ; E.kanan = G
+##G.kiri = H
+##B.kiri = I
+##
+##def cetakLuarKiri(data):
+##    if data is not None:
+##        cetakLuarKiri(data.kiri)
+##        print(data.data)
+##def cetakLuarKanan(data):
+##    if data is not None:
+##        print(data.data)
+##        cetakLuarKanan(data.kanan)
+##        
+##cetakLuarKiri(A)
+##cetakLuarKanan(A)
